610675e0abc9a4b3bc92894316b76942206199ac
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-09-08 14:55:18 -0400

    base folder structure

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+const bodyParser =require('body-parser');
+const cookieParser =require('cookie-parser');
+
+require("dotenv").config();
+
+// import routes
+const userRoutes = require('./routes/user');
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log(`Db connection successfully established!`);
+});
+mongoose.connection.on('error', err =>{
+    console.log(`Db connection error: ${err.message}`);
+});
+
+
+// middlewares 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// routes middleware
+app.use('/api',userRoutes);
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
